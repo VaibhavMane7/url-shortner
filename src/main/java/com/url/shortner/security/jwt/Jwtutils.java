0beeb12f5c1ade31033a1daffecebbb3035fbde7
Vaibhav Mane <manevaibhav981@gmail.com>
2025-09-13 18:55:10 +0530

    Changes made

--- src/main/java/com/url/shortner/security/jwt/Jwtutils.java
+++ src/main/java/com/url/shortner/security/jwt/Jwtutils.java
@@ -1,7 +1,10 @@
 package com.url.shortner.security.jwt;
 
 import com.url.shortner.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.stream.Collectors;
 
 public class Jwtutils {
     public String getJwtHeader(HttpServletRequest request){
@@ -14,6 +17,7 @@ public class Jwtutils {
 
     public String generateToken(UserDetailsImpl userDetails){
         String username=userDetails.getUsername();
-        String roles=userDetails.getAuthorities().stream()
+        String roles=userDetails.getAuthorities().stream().map(authority->authority.getAuthority()).collect(Collectors.joining(","));
+        return Jwts.builder().subject(username).claim("roles",roles)
     }
 }
